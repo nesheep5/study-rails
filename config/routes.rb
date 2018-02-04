6169d22bf24d4dfd9c68bde2283346404cbeeb3e
shogo watanabe <ne.sheep.5mouths@gmail.com>
2018-02-04 19:49:37 +0900

    form_tagについて整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello' => 'hello#index'
   get 'hello/view' => 'hello#view'
 
   get 'view/keyword'
   post 'keyword/search'
+  get 'view/form_tag'
+  post 'view/create'
 end
